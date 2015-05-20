5058b6047c0a13f35e39f4073b61480f2ec11909
xiaohehu <xiaohe.hu@bos-mini-3.local>
2015-05-20 10:18:15 -0400

    Make collection view and load blank detail view

--- brochureApp/ViewController.m
+++ brochureApp/ViewController.m
@@ -7,16 +7,60 @@
 //
 
 #import "ViewController.h"
+#import "DetailViewController.h"
+@interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
 
-@interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UICollectionView *uic_mainCollection;
+@property (nonatomic, strong) DetailViewController    *detail_vc;
 @end
 
 @implementation ViewController
 
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _uic_mainCollection.delegate = self;
+    _uic_mainCollection.dataSource = self;
+    [_uic_mainCollection setContentInset:UIEdgeInsetsMake(20.0, 20.0, 0.0, 20.0)];
+}
+
+#pragma mark - Collection Delegate Methods
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                 cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *galleryCell = [collectionView
+                                       dequeueReusableCellWithReuseIdentifier:@"myCell"
+                                       forIndexPath:indexPath];
+    return galleryCell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    if (_detail_vc != nil) {
+        [_detail_vc.view removeFromSuperview];
+        [_detail_vc removeFromParentViewController];
+        _detail_vc = nil;
+    }
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    _detail_vc = [storyboard instantiateViewControllerWithIdentifier:@"DetailViewController"];
+    _detail_vc.view.frame = self.view.bounds;
+    [self.view addSubview: _detail_vc.view];
 }
 
 - (void)didReceiveMemoryWarning {
